051362c69a5b978ab28a1e6ff75cf1c60c8a2229
mirzamitdinovs <sayyodmirzomitdinov@gmail.com>
2022-03-22 10:09:12 +0500

    navbar fixed

--- pages/sites/index.jsx
+++ pages/sites/index.jsx
@@ -1,10 +1,30 @@
+import { useEffect } from 'react';
+import $ from 'jquery';
 const ExternalPages = () => {
 	return (
-		<div>
+		<div
+			style={{
+				margin: 0,
+				padding: 0,
+				height: '100vh',
+				overflow: 'hidden',
+				overFloY: 'hidden',
+			}}
+		>
 			<iframe
+				id='frame'
 				src='https://www.education.com/games/math/'
 				width='100%'
-				style={{ height: '100vh', marginTop: '-100px' }}
+				style={{
+					marginTop: '-100px',
+					border: 'none',
+					width: '100%',
+					height: 'calc(100% + 100px)',
+					position: 'absolute',
+				}}
+				// srcDoc='<footer><h1>Hello</h1></footer>'
+				// sandbox='allow-scripts allow-modal'
+				loading='eager'
 			></iframe>
 		</div>
 	);
